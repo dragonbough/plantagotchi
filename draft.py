e7e1509887c1f43b86a38228d767af4fa58ad98d
Bright <115483085+Brightpea067@users.noreply.github.com>
2025-03-07 09:13:36 +0000

    Create draft.py

---
+++ draft.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+
+#initializes the clock and the game screen
+
+running = True
+
+player_pos =  pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2) #sets the player position to the middle of the screen
+
+while running:
+        pygame.display.flip() #update display (required to see changes made on the screen)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        running = False #checks through the events registered, if the game is quit pygame stops
+        screen.fill("purple")
+        pygame.draw.circle(screen, "red", player_pos, 40)
