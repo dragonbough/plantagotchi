f34acd20b62f70e7fe34317c44805032e78fb3d3
dragonbough <fiveteen81@gmail.com>
2025-03-08 22:01:15 +0000

    refactored classes and switched to PyGame Sprite/Group functionalities
    -created generic GameSprite class
    -created AnimSprite class ==> child of GameSprite class
    -created RenderQueue class used for rendering objects
    + more

--- draft.py
+++ draft.py
@@ -1,50 +1,83 @@
 import pygame
 import os
+import pickle
 
 pygame.init()
 
 def return_frames_count(directory):
         return len(os.listdir(directory)) 
 
-size = width, height = 300, 300
-screen = pygame.display.set_mode(size)
+screen_size = screen_width, screen_height = 300, 300
+screen = pygame.display.set_mode(screen_size)
+screen_center = screen_width / 2, screen_height /2 
 clock = pygame.time.Clock()
+                
+class RenderQueue(pygame.sprite.Group):
+        def __init__(self, *args):
+                pygame.sprite.RenderUpdates.__init__(self, *args)
 
-plant_folders = {"daisy":"daisy_frames"} # put plant names and their sprite folder in this dict
+on_screen = RenderQueue()
 
-#plant class -- this contains the functionality that allows for animation
-class Plant:
-        def __init__(self, name, file_format=".png"):
+# --- plant class --- this contains the functionality that allows for animation
+class GameSprite(pygame.sprite.Sprite):
+        def __init__(self, name, size=(50, 50), position=(screen_center), file_format=".png"):
+                pygame.sprite.Sprite.__init__(self)
                 self.name = name
                 self.file_format = file_format
-                self.directory = f"sprites/{plant_folders[self.name]}/"
-                self.frame = 1
-                self.max_frame = return_frames_count(self.directory)
-                self.sprite = pygame.image.load(self.directory + str(self.frame) + self.file_format).convert()
-                self.size = (100, 100)
-                pygame.transform.scale(self.sprite, self.size)
-                self.rect = self.sprite.get_rect()
+                self.directory = f"sprites/{self.name}/"
+                print(self.directory)
+                self.size = size
+                self.image = pygame.transform.scale(pygame.image.load(self.directory + str(self.name) + self.file_format).convert_alpha(), self.size)
+                self.position = position
+                self.rect = self.image.get_rect()
+                self.rect.center = self.position
+                
+        def set_image(self):
+                self.image = pygame.transform.scale(pygame.image.load(self.directory + str(self.name) + self.file_format).convert_alpha(), self.size)
+                self.rect = self.image.get_rect()
+                self.rect.center = self.position
                 
         def update_frame(self):
-                if self.frame == self.max_frame:
+                pass
+                
+class AnimSprite(GameSprite):
+        def __init__(self, *args):
+                super().__init__(*args)
+                self.frame = 1
+                
+        def set_image(self):
+                self.image = pygame.transform.scale(pygame.image.load(self.directory + str(self.frame) + self.file_format).convert_alpha(), self.size) #uses frames instead of name
+                self.rect = self.image.get_rect()
+                self.rect.center = self.position
+                
+        def update_frame(self):
+                max_frame = return_frames_count(self.directory) - 1 # -1 as we are not considering the image with the plain name
+                if self.frame == max_frame:
                         self.frame = 1
                 else:
                         self.frame += 1 
-                self.sprite = pygame.image.load(self.directory + str(self.frame) + self.file_format).convert()
-                screen.blit(self.sprite, self.rect)
+                self.set_image()
+                on_screen.remove([sprite for sprite in on_screen if sprite.name == self.name]) #only one of this plant can be on screen at a time
+                on_screen.add(self)
                 
+                
+class UIElement(GameSprite): 
+        def __init__(self, name, size, position, file_format):
+                self.directory = "sprites/ui"
+                
+                
+current = AnimSprite("daisy", (150, 150)) #defining the current plant as a Plant object named "daisy"
 running = True
-
-current = Plant("daisy") #defining the current selected plant as a Plant object named "daisy"
 
 while running:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        running = False #
+                        running = False
                 
         #main rendering stuff goes here:
-        screen.fill("purple")
-        current.update_frame() #updates the current plant object's animation 
+        screen.fill("purple") #background
+        current.update_frame() #updates the current plant object's animation and adds it to on_screen RenderQueue group
+        on_screen.draw(screen) #draws all objects in the on_screen group 
         pygame.display.flip() #update display (required to see changes made on the screen)
         
         clock.tick(10) #limits game to 5fps -- i need to keep the game at decent fps while also limiting fps of animation, how?
